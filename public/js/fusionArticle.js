d94f127067f940d2887a93d349e5ba06cfbe80ac
Léo Grouet <leogrouet@MacBook-Air-de-Leo.local>
2023-10-18 18:41:57 +0200

    Animation JS ok

--- public/js/fusionArticle.js
+++ public/js/fusionArticle.js
@@ -7,6 +7,9 @@ articles.forEach((article) => {
     // Récupère l'élément spécifique qui a été cliqué
     const clickedArticle = event.currentTarget;
     const textElement = clickedArticle.querySelector(".article_overview_title");
+    const textTitle = clickedArticle.querySelector(
+      ".article_overview_title_text"
+    );
     const descriptionDiv = clickedArticle.querySelector(
       ".article_overview_description"
     );
@@ -21,20 +24,24 @@ articles.forEach((article) => {
           // Rétablir l'état initial
           article.style.width = "15%";
           textElement.style.position = "absolute";
+          textElement.style.height = "10%";
           article.classList.add("anim");
           descriptionDiv.style.display = "none";
+          textTitle.style.padding = "5px 5px 5px 5px";
+          article.style.borderRadius = "50% 50% 50% 50% / 15% 15% 15% 15%";
         } else {
           // Augmente la largeur de l'article sélectionné
           article.style.width = "100%";
+          article.style.borderRadius = "0";
           article.style.transition = "1s";
           article.classList.remove("anim");
+          textTitle.style.padding = "38px 5px 5px 5px";
           descriptionDiv.style.display = "block";
-          textElement.style.position = "static";
-          textElement.style.top = "auto";
+          textElement.style.position = "absolute";
           textElement.style.left = "0";
           textElement.style.bottom = "0"; // Ajustez la position pour rester en bas
           textElement.style.width = "100%";
-          textElement.style.height = "10%";
+          textElement.style.height = "100%";
           textElement.style.zIndex = "2"; // Assurez-vous que le texte est au-dessus de l'image de fond
         }
       } else {
