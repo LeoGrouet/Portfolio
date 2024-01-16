8911b5d552af0ec251d07eb68c64960c16eda43c
LeoGrouet <leo.grouet@gmail.com>
2024-01-16 11:21:29 +0100

    maj css

--- public/js/animationArticle.js
+++ public/js/animationArticle.js
@@ -1,2 +1,61 @@
-// Je selectionne l'ensemble de mes articles
 const articles = document.querySelectorAll(".article_overview");
+
+articles.forEach((article) => {
+  article.addEventListener("click", articleExtend);
+
+  function articleExtend(event) {
+    // Récupère l'élément spécifique qui a été cliqué
+    const clickedArticle = event.currentTarget;
+    const textElement = clickedArticle.querySelector(".article_overview_title");
+    const textTitle = clickedArticle.querySelector(
+      ".article_overview_title_text"
+    );
+    const descriptionDiv = clickedArticle.querySelector(
+      ".article_overview_description"
+    );
+
+    // Vérifie si l'article est déjà étendu
+    const isExpanded = clickedArticle.style.width === "100%";
+
+    // Parcours tous les articles
+    articles.forEach((article) => {
+      const backArrow = article.querySelector(".back_arrow");
+      // Si l'article est séléctionné
+      if (article === clickedArticle) {
+        // Et que cet article est déja étendu
+        if (isExpanded) {
+          // Rétablir l'état initial
+          article.style.width = "15%";
+          textElement.style.position = "absolute";
+          article.style.transition = "0.4s";
+          textElement.style.height = "10%";
+          article.classList.add("anim");
+          descriptionDiv.style.display = "none";
+          textTitle.style.padding = "5px 5px 5px 5px";
+          article.style.borderRadius = "50% 50% 50% 50% / 15% 15% 15% 15%";
+        } else {
+          // Augmente la largeur de l'article sélectionné
+          article.style.width = "100%";
+          article.style.borderRadius = "0";
+          article.style.transition = "1s";
+          article.classList.remove("anim");
+          textTitle.style.padding = "38px 5px 5px 5px";
+          if (clickedArticle.id != "third") {
+            descriptionDiv.style.display = "block";
+          } else {
+            descriptionDiv.style.display = "flex";
+          }
+          textElement.style.position = "absolute";
+          textElement.style.left = "0";
+          textElement.style.bottom = "0"; // Ajustez la position pour rester en bas
+          textElement.style.width = "100%";
+          textElement.style.height = "95%";
+          textElement.style.zIndex = "2"; // Assurez-vous que le texte est au-dessus de l'image de fond
+        }
+      } else {
+        // Cache les articles non sélectionnés
+        article.style.display = isExpanded ? "block" : "none";
+      }
+    });
+  }
+});
--- public/js/cardTech.js
+++ public/js/cardTech.js
@@ -1,39 +1,58 @@
 const techsArray = [
   {
     name: "Javascript",
+    context: "Formation O'Clock",
     image: "./image/logoStacksTechniques/jslogo.png",
   },
   {
     name: "HTML",
+    context: "Formation O'Clock",
     image: "./image/logoStacksTechniques/html-5.png",
   },
   {
     name: "CSS",
+    context: "Formation O'Clock",
     image: "./image/logoStacksTechniques/css-3.png",
   },
   {
     name: "EJS",
+    context: "Formation O'Clock",
     image: "./image/logoStacksTechniques/ejslogo.png",
   },
   {
     name: "MONGO DB",
+    context: "Auto-apprentissage",
     image: "./image/logoStacksTechniques/MONGODB.webp",
   },
   {
     name: "NODE.JS",
+    context: "Formation O'Clock",
     image: "./image/logoStacksTechniques/node-js.png",
   },
   {
     name: "SQL",
+    context: "Formation O'Clock",
     image: "./image/logoStacksTechniques/sqllogo.jpg",
   },
   {
     name: "WHIMSICAL",
+    context: "Auto-apprentissage",
     image: "./image/logoStacksTechniques/whimsical.webp",
   },
   {
     name: "Postgres",
+    context: "Formation O'Clock",
     image: "./image/logoStacksTechniques/Postgresql_elephant.png",
+  },
+  {
+    name: "Angular",
+    context: "Auto-apprentissage en cours",
+    image: "./image/logoStacksTechniques/angular.png",
+  },
+  {
+    name: "React",
+    context: "Auto-apprentissage en cours",
+    image: "./image/logoStacksTechniques/react.png",
   },
 ];
 
@@ -41,8 +60,11 @@ const cardContainer = document.querySelector(".card-tech-container");
 
 for (const card of techsArray) {
   const cardDiv = document.createElement("div");
+  const cardDivContext = document.createElement("p");
   cardDiv.classList.add("card-tech");
   cardDiv.innerHTML = card.name;
+  cardDivContext.innerHTML = card.context;
+  cardDivContext.style.display = "flex";
 
   // Créer l'élément img et définir son attribut src
   const imgDiv = document.createElement("img");
@@ -50,5 +72,6 @@ for (const card of techsArray) {
 
   // Ajouter les éléments au conteneur
   cardDiv.appendChild(imgDiv);
+  cardDiv.appendChild(cardDivContext);
   cardContainer.appendChild(cardDiv);
 }
