f1a0d11c80de8f30abfe278939a2108fe688bd9e
LeoGrouet <leo.grouet@gmail.com>
2024-01-23 16:00:03 +0100

    chore: New initial commit. Start new behavior on git workflow.

--- public/js/animationArticle.js
+++ public/js/animationArticle.js
@@ -1,61 +1,44 @@
-const articles = document.querySelectorAll(".article_overview");
+// Tableau d'objet comprenant l'ensemble des articles selon leurs ID
+const articlesArray = [
+  {
+    content: document.getElementById("first"),
+  },
+  {
+    content: document.getElementById("second"),
+  },
+  {
+    content: document.getElementById("third"),
+  },
+  {
+    content: document.getElementById("fourth"),
+  },
+];
 
-articles.forEach((article) => {
-  article.addEventListener("click", articleExtend);
-
-  function articleExtend(event) {
-    // Récupère l'élément spécifique qui a été cliqué
+// Pour chaque article de ce tableau, lors d'un clic sur
+for (const article of articlesArray) {
+  article.content.addEventListener("click", (event) => {
+    // On isole l'article séléctionné
     const clickedArticle = event.currentTarget;
-    const textElement = clickedArticle.querySelector(".article_overview_title");
-    const textTitle = clickedArticle.querySelector(
-      ".article_overview_title_text"
-    );
-    const descriptionDiv = clickedArticle.querySelector(
+    // On isole également les div interne a l'article
+    const title = clickedArticle.querySelector(".article_overview_title");
+    const description = clickedArticle.querySelector(
       ".article_overview_description"
     );
 
-    // Vérifie si l'article est déjà étendu
-    const isExpanded = clickedArticle.style.width === "100%";
+    // Reste du code pour agrandir l'article lorsqu'il est cliqué
+    clickedArticle.style.width = "100%";
+    clickedArticle.style.borderRadius = "0%";
+    clickedArticle.style.transform = "none";
+    description.style.display = "flex";
+    title.style.height = "91%";
 
-    // Parcours tous les articles
-    articles.forEach((article) => {
-      const backArrow = article.querySelector(".back_arrow");
-      // Si l'article est séléctionné
-      if (article === clickedArticle) {
-        // Et que cet article est déja étendu
-        if (isExpanded) {
-          // Rétablir l'état initial
-          article.style.width = "15%";
-          textElement.style.position = "absolute";
-          article.style.transition = "0.4s";
-          textElement.style.height = "10%";
-          article.classList.add("anim");
-          descriptionDiv.style.display = "none";
-          textTitle.style.padding = "5px 5px 5px 5px";
-          article.style.borderRadius = "50% 50% 50% 50% / 15% 15% 15% 15%";
-        } else {
-          // Augmente la largeur de l'article sélectionné
-          article.style.width = "100%";
-          article.style.borderRadius = "0";
-          article.style.transition = "1s";
-          article.classList.remove("anim");
-          textTitle.style.padding = "38px 5px 5px 5px";
-          if (clickedArticle.id != "third") {
-            descriptionDiv.style.display = "block";
-          } else {
-            descriptionDiv.style.display = "flex";
-          }
-          textElement.style.position = "absolute";
-          textElement.style.left = "0";
-          textElement.style.bottom = "0"; // Ajustez la position pour rester en bas
-          textElement.style.width = "100%";
-          textElement.style.height = "95%";
-          textElement.style.zIndex = "2"; // Assurez-vous que le texte est au-dessus de l'image de fond
-        }
-      } else {
-        // Cache les articles non sélectionnés
-        article.style.display = isExpanded ? "block" : "none";
+    // On récupere également les autres articles, qu'on va display : none
+    articlesArray.forEach(function (otherArticle) {
+      if (otherArticle.content !== clickedArticle) {
+        const others = otherArticle.content;
+        // Vous pouvez ajouter du code ici pour masquer les autres articles si nécessaire
+        others.style.display = "none";
       }
     });
-  }
-});
+  });
+}
